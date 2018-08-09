16f6cd9c9789ef5f38a22f6d0e7c8b0021324543
jcarklin <jcarklin@gmail.com>
2018-08-09 11:03:24 +0200

    Added Null checks

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -16,23 +16,29 @@ public class JsonUtils {
     public static Sandwich parseSandwichJson(String json) {
 
         try {
-
+            String mainName="", description = "", placeOfOrigin = "", image="";
+            List<String> alsoKnownAsList=new ArrayList<>(), ingredientsList = new ArrayList<>();
             JSONObject sandwichDetailsJsonObject = new JSONObject(json);
-
-            JSONObject jsonObject = sandwichDetailsJsonObject.optJSONObject("name");
-            String mainName = jsonObject.optString("mainName");
-            JSONArray jsonArray = jsonObject.optJSONArray("alsoKnownAs");
-            List<String> alsoKnownAsList = new ArrayList<>();
-            for (int i=0; i<jsonArray.length();i++) {
-                alsoKnownAsList.add(jsonArray.optString(i));
-            }
-            String placeOfOrigin = sandwichDetailsJsonObject.optString("placeOfOrigin");
-            String description = sandwichDetailsJsonObject.optString("description");
-            String image = sandwichDetailsJsonObject.optString("image");
-            jsonArray = sandwichDetailsJsonObject.optJSONArray("ingredients");
-            List<String> ingredientsList = new ArrayList<>();
-            for (int i=0; i<jsonArray.length();i++) {
-                ingredientsList.add(jsonArray.optString(i));
+            if (sandwichDetailsJsonObject != null) {
+                JSONObject jsonObject = sandwichDetailsJsonObject.optJSONObject("name");
+                if (jsonObject != null) {
+                    mainName = jsonObject.optString("mainName", "");
+                }
+                JSONArray jsonArray = jsonObject.optJSONArray("alsoKnownAs");
+                if (jsonArray != null) {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        alsoKnownAsList.add(jsonArray.optString(i));
+                    }
+                }
+                placeOfOrigin = sandwichDetailsJsonObject.optString("placeOfOrigin","");
+                description = sandwichDetailsJsonObject.optString("description","");
+                image = sandwichDetailsJsonObject.optString("image","");
+                jsonArray = sandwichDetailsJsonObject.optJSONArray("ingredients");
+                if (jsonArray != null) {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        ingredientsList.add(jsonArray.optString(i));
+                    }
+                }
             }
             return new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
 
